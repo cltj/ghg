6121fce2732c322ea6dbe302fcae749f2c63a6bd
cltj <tj@cloudlink.ai>
2024-11-04 11:41:14 +0100

    added exploratory excel to docs

---
+++ src/model_generator/calculate_outputs.py
@@ -0,0 +1,38 @@
+# This file is used to calculate the outputs of the model. 
+
+def fossil_fuel_output():
+    return "NA" # in metric tons of CO2e
+
+
+def ch4_output():
+    return "NA" # in kilograms of ch4
+
+
+def n2o_output():
+    return "NA" # in kilograms of n2o
+
+
+def total_ghg_emissions_excluding_biofuel_co2():
+    return "NA" # in metric tons of CO2e
+
+
+def biofuel_co2_emissions():
+    return "NA" # in metric tons of CO2e
+
+
+def applied_co2_emission_factor():
+    return "NA" # in kilograms of CO2e per litre
+
+
+def applied_ch4_emission_factor():
+    return "NA" # in grams of ch4 per kilometer
+
+
+def applied_n2o_emission_factor():
+    return "NA" # in grams of n2o per kilometer
+
+
+def applied_biofuel_co2_emission_factor():
+    return "NA" # in ???
+
+
